f03d31f39dc59d41ae15eb859b1ec9ffbf92f1fb
aymen.raji@remotecoders.org <aymen.raji@remotecoders.org>
2024-05-13 08:14:12 +0000

    stg_strip_payment was added and source was added for both jaffle_shop and stripe

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,7 @@ select
     orderid as Order_id,
     paymentmethod as payment_method,
     status,
+    -- amount is stored in cents, convert it to dollars
     amount / 100 as Amount,
     created as created_at
-from raw.stripe.payment
+from {{ source('stripe', 'payment') }} 
